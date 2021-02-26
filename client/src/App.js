65c29742e56bd787ad62dd653d588e5d341a67f8
Michail Guzhuman <mguzhuman@brainreign.pro>
2021-02-26 09:59:30 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 import Main from "./Main";
 import socketConnect from 'socket.io-client';
 import React, {useEffect, useState, Fragment} from "react";
-import  fs  from "fs";
+//import  fs  from "fs";
 var socket;
 
 const App = () => {
     const [rooms, setRooms] = useState([]);
     const [isConnect, setIsConnect] = useState(false);
     useEffect(() => {
-        socket = socketConnect({
-            ca: fs.readFileSync("/etc/letsencrypt/live/codetalk.pro/cert.pem")
-        })
+        socket = socketConnect()
         socket.on('listRooms', (data) => {
             setIsConnect(true);
             setRooms(data);
